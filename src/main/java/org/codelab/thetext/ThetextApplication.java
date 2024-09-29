9949441d3950196fda7723d9a71270e7812086cd
SirShiro1046 <myandroid146@gmail.com>
2024-09-29 09:34:39 -0600

    primer commit

---
+++ src/main/java/org/codelab/thetext/ThetextApplication.java
@@ -0,0 +1,13 @@
+package org.codelab.thetext;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThetextApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ThetextApplication.class, args);
+    }
+
+}
